ece48dcd5b08bc4305c74733f4b4769256385b0a
Marco Biasone <marco.biasone.90@gmail.com>
2022-10-20 16:23:01 +0200

    Initial version of helpMeChef generated by generator-jhipster@7.8.1

---
+++ src/main/java/it/marcobiasone/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package it.marcobiasone.domain;
---
+++ src/main/java/it/marcobiasone/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package it.marcobiasone.web.rest.vm;
