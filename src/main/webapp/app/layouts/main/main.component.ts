bb81a699f2f065b5499f7d46c3edec9a66788a7c
Marco Biasone <marco.biasone.90@gmail.com>
2022-10-22 13:03:35 +0200

    vertical navbar

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -3,20 +3,25 @@ import { Title } from '@angular/platform-browser';
 import { Router, ActivatedRouteSnapshot, NavigationEnd } from '@angular/router';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import dayjs from 'dayjs/esm';
+import { Account } from 'app/core/auth/account.model';
 
 import { AccountService } from 'app/core/auth/account.service';
+import { LoginService } from 'app/login/login.service';
 
 @Component({
   selector: 'jhi-main',
   templateUrl: './main.component.html',
 })
 export class MainComponent implements OnInit {
+  public account: Account | null = null;
+
   private renderer: Renderer2;
 
   constructor(
     private accountService: AccountService,
     private titleService: Title,
     private router: Router,
+    private loginService: LoginService,
     private translateService: TranslateService,
     rootRenderer: RendererFactory2
   ) {
@@ -26,6 +31,10 @@ export class MainComponent implements OnInit {
   ngOnInit(): void {
     // try to log in automatically
     this.accountService.identity().subscribe();
+
+    this.accountService.getAuthenticationState().subscribe(account => {
+      this.account = account;
+    });
 
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
@@ -38,6 +47,10 @@ export class MainComponent implements OnInit {
       dayjs.locale(langChangeEvent.lang);
       this.renderer.setAttribute(document.querySelector('html'), 'lang', langChangeEvent.lang);
     });
+  }
+
+  login(): void {
+    this.loginService.login();
   }
 
   private getPageTitle(routeSnapshot: ActivatedRouteSnapshot): string {
