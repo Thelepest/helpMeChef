2532080f4f05611b5142bbe5afa0b3c507a24eb6
Marco Biasone <marco.biasone.90@gmail.com>
2022-11-07 17:06:28 +0100

    updates on user homepage

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'app/login/login.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'jhi-home',
@@ -12,7 +13,9 @@ import { Account } from 'app/core/auth/account.model';
 export class HomeComponent implements OnInit {
   account: Account | null = null;
 
-  constructor(private accountService: AccountService, private loginService: LoginService) {}
+  constructor(private accountService: AccountService, private loginService: LoginService,
+    protected activatedRoute: ActivatedRoute,
+    private router: Router) {}
 
   ngOnInit(): void {
     this.accountService.identity().subscribe(account => (this.account = account));
@@ -20,5 +23,9 @@ export class HomeComponent implements OnInit {
 
   login(): void {
     this.loginService.login();
+  }
+
+  goToUserPage(): void {
+    this.router.navigate(['/pantry']);
   }
 }
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [ButtonModule, SharedModule, RouterModule.forChild([HOME_ROUTE])],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
